3baeff308aee66090d32913e3a4dacc9a88d4093
Philipp Scheer <scheer28philipp@gmail.com>
2026-01-08 19:44:27 +0100

    print profile result for debugging

--- models/run_tests.py
+++ models/run_tests.py
@@ -34,14 +34,30 @@ logger.add(lambda msg: tqdm.write(msg, end=""), colorize=True, level="DEBUG")
 CURRENT_PATH = os.path.dirname(os.path.abspath(__file__))
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Run scheduling algorithm tests.")
-    parser.add_argument("-r", "--rounds", type=int, help="Number of rounds every test runs", default=1)
+    parser = argparse.ArgumentParser(
+        description="Run scheduling algorithm tests."
+    )
+    parser.add_argument(
+        "-r",
+        "--rounds",
+        type=int,
+        help="Number of rounds every test runs",
+        default=1,
+    )
     parser.add_argument("--ilp", action="store_true", help="Run ILP tests")
     parser.add_argument("--ilp-gpu", action="store_true", help="Run ILP tests")
-    parser.add_argument("--hc", action="store_true", help="Run Hill Climbing tests (v1 and v3)")
-    parser.add_argument("--oo", action="store_true", help="Run Offering Order test")
-    parser.add_argument("--config", help="Config ID", default=None, required=False)
-    parser.add_argument("--print-schedule", action="store_true", help="Print the found schedule")
+    parser.add_argument(
+        "--hc", action="store_true", help="Run Hill Climbing tests (v1 and v3)"
+    )
+    parser.add_argument(
+        "--oo", action="store_true", help="Run Offering Order test"
+    )
+    parser.add_argument(
+        "--config", help="Config ID", default=None, required=False
+    )
+    parser.add_argument(
+        "--print-schedule", action="store_true", help="Print the found schedule"
+    )
     parser.add_argument(
         "--course-min",
         type=int,
@@ -82,7 +98,12 @@ if __name__ == "__main__":
 
     logger.debug(f"found {len(config_files)} config files")
 
-    with tqdm(config_files, desc="Benchmarking constraint config", position=1, leave=True) as pbar:
+    with tqdm(
+        config_files,
+        desc="Benchmarking constraint config",
+        position=1,
+        leave=True,
+    ) as pbar:
         for cfg_path in pbar:
             cfg_path = Path(cfg_path)
             cfg_name = cfg_path.stem  # e.g. constraint1
@@ -91,34 +112,58 @@ if __name__ == "__main__":
             offerings = preprocess(all_offerings)
 
             if args.course_min and args.course_max:
-                C.TOTAL_COURSE_COUNT_CONSTRAINT = SimpleNamespace(min=args.course_min, max=args.course_max)
-                logger.info(f"Set TOTAL_COURSE_COUNT_CONSTRAINT to {C.TOTAL_COURSE_COUNT_CONSTRAINT}")
+                C.TOTAL_COURSE_COUNT_CONSTRAINT = SimpleNamespace(
+                    min=args.course_min, max=args.course_max
+                )
+                logger.info(
+                    f"Set TOTAL_COURSE_COUNT_CONSTRAINT to {C.TOTAL_COURSE_COUNT_CONSTRAINT}"
+                )
 
             elif args.course_min or args.course_max:
                 logger.warning("both --course-min and --course-max must be set")
                 exit(1)
 
             elif cfg.get("TOTAL_COURSE_COUNT_CONSTRAINT") is None:
-                logger.warning("no course count constraint specified, testing max course count using ilp")
-                C.TOTAL_COURSE_COUNT_CONSTRAINT = SimpleNamespace(min=1, max=999)
+                logger.warning(
+                    "no course count constraint specified, testing max course count using ilp"
+                )
+                C.TOTAL_COURSE_COUNT_CONSTRAINT = SimpleNamespace(
+                    min=1, max=999
+                )
                 logger.info(f"trying count {C.TOTAL_COURSE_COUNT_CONSTRAINT}")
-                schedule = test_ilp(offerings)  # might be the best schedule, but not the longest
+                schedule = test_ilp(
+                    offerings
+                )  # might be the best schedule, but not the longest
                 previous_schedule = None
 
                 while is_valid_schedule(schedule, schedule_complete=True):
                     previous_schedule = schedule
-                    C.TOTAL_COURSE_COUNT_CONSTRAINT = SimpleNamespace(min=len(schedule) + 1, max=999)
-                    logger.info(f"trying count {C.TOTAL_COURSE_COUNT_CONSTRAINT}")
+                    C.TOTAL_COURSE_COUNT_CONSTRAINT = SimpleNamespace(
+                        min=len(schedule) + 1, max=999
+                    )
+                    logger.info(
+                        f"trying count {C.TOTAL_COURSE_COUNT_CONSTRAINT}"
+                    )
                     schedule = test_ilp(offerings)
                     logger.info(f"result {schedule}")
                     logger.info(f"length {len(schedule)}")
-                    logger.info(f"is valid? {is_valid_schedule(schedule, schedule_complete=True)}")
+                    logger.info(
+                        f"is valid? {is_valid_schedule(schedule, schedule_complete=True)}"
+                    )
                     logger.info(f"score: {get_schedule_mark(schedule)}")
 
-                C.TOTAL_COURSE_COUNT_CONSTRAINT = SimpleNamespace(min=len(previous_schedule), max=999)
-                logger.success(f"found longest ilp schedule with length {len(previous_schedule)})")
-                logger.success(f"is valid? {is_valid_schedule(previous_schedule, schedule_complete=True)}")
-                C.TOTAL_COURSE_COUNT_CONSTRAINT = SimpleNamespace(min=1, max=len(previous_schedule))
+                C.TOTAL_COURSE_COUNT_CONSTRAINT = SimpleNamespace(
+                    min=len(previous_schedule), max=999
+                )
+                logger.success(
+                    f"found longest ilp schedule with length {len(previous_schedule)})"
+                )
+                logger.success(
+                    f"is valid? {is_valid_schedule(previous_schedule, schedule_complete=True)}"
+                )
+                C.TOTAL_COURSE_COUNT_CONSTRAINT = SimpleNamespace(
+                    min=1, max=len(previous_schedule)
+                )
 
             cfg_title = cfg.get("title", cfg_name)
 
@@ -126,13 +171,20 @@ if __name__ == "__main__":
 
             pbar.set_postfix_str(f"Benchmarking {cfg_title}")
 
-            test_results = {"ilp": [], "offering_order": [], "hill_climbing_v1": [], "hill_climbing_v3": []}
+            test_results = {
+                "ilp": [],
+                "offering_order": [],
+                "hill_climbing_v1": [],
+                "hill_climbing_v3": [],
+            }
 
             ccc_min = C.TOTAL_COURSE_COUNT_CONSTRAINT.min
             ccc_max = C.TOTAL_COURSE_COUNT_CONSTRAINT.max + 1
 
             for i in range(ccc_min, ccc_max):
-                C.TOTAL_COURSE_COUNT_CONSTRAINT = SimpleNamespace(min=i, max=i)  # fix course count
+                C.TOTAL_COURSE_COUNT_CONSTRAINT = SimpleNamespace(
+                    min=i, max=i
+                )  # fix course count
 
                 for alg_name, alg_fn in tests.items():
                     profiles: list[ProfileResult] = []
@@ -140,7 +192,9 @@ if __name__ == "__main__":
                     schedule: list[Offering] | None = None
 
                     for _ in range(loops):
-                        pbar.set_postfix_str(f"{cfg_title}, ccc {i}/{ccc_max-1}, run {_+1}/{loops}")
+                        pbar.set_postfix_str(
+                            f"{cfg_title}, ccc {i}/{ccc_max-1}, run {_+1}/{loops}"
+                        )
 
                         with profile() as profileResult:
                             schedule = alg_fn(offerings)
@@ -150,7 +204,12 @@ if __name__ == "__main__":
 
                         profiles.append(profileResult)
 
-                    all_measurements = [m for p in profiles for m in p.mem_measurements]
+                    if loops == 1 and args.print_schedule:
+                        logger.debug(f"measurement: {profiles[0]}")
+
+                    all_measurements = [
+                        m for p in profiles for m in p.mem_measurements
+                    ]
 
                     logger.success(
                         f"schedule={[str(c.courseId) + ':' + c.groupId for c in (schedule or [])]}, is valid? {is_valid_schedule(schedule, schedule_complete=True)}, mark: {get_schedule_mark(schedule)}"
@@ -160,18 +219,34 @@ if __name__ == "__main__":
                         {
                             "courses": i,
                             "score": get_schedule_mark(schedule),
-                            "valid": is_valid_schedule(schedule, schedule_complete=True),
+                            "valid": is_valid_schedule(
+                                schedule, schedule_complete=True
+                            ),
                             # run timings
                             "timings": [p.time_elapsed for p in profiles],
-                            "timings_mean": statistics.mean(p.time_elapsed for p in profiles),
-                            "timings_median": statistics.median(p.time_elapsed for p in profiles),
+                            "timings_mean": statistics.mean(
+                                p.time_elapsed for p in profiles
+                            ),
+                            "timings_median": statistics.median(
+                                p.time_elapsed for p in profiles
+                            ),
                             "timings_stdev": (
-                                statistics.stdev(p.time_elapsed for p in profiles) if len(profiles) > 1 else 0
+                                statistics.stdev(
+                                    p.time_elapsed for p in profiles
+                                )
+                                if len(profiles) > 1
+                                else 0
                             ),
                             # memory profiling
                             "memory_mean": statistics.mean(all_measurements),
-                            "memory_median": statistics.median(all_measurements),
-                            "memory_stdev": statistics.stdev(all_measurements) if len(all_measurements) > 1 else 0,
+                            "memory_median": statistics.median(
+                                all_measurements
+                            ),
+                            "memory_stdev": (
+                                statistics.stdev(all_measurements)
+                                if len(all_measurements) > 1
+                                else 0
+                            ),
                             "memory_min": min(all_measurements),
                             "memory_max": max(all_measurements),
                             "memory_peak": max(p.mem_peak for p in profiles),
