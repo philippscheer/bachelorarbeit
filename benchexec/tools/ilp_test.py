a7d09bf673d39013de0019e8d7bd7e2c0626a062
Philipp Scheer <scheer28philipp@gmail.com>
2026-02-19 14:38:12 +0100

    use venv

--- benchexec/tools/ilp_test.py
+++ benchexec/tools/ilp_test.py
@@ -53,7 +53,7 @@ class Tool(benchexec.tools.template.BaseTool):
         """
         # Intentionally leave out 'options' to ensure ONLY the file is passed.
         # This will execute: python3 /path/to/ilp_test.py /path/to/constraint.json
-        return ["python3", executable] + tasks
+        return [".venv/bin/python3", executable] + tasks
 
     def determine_result(self, returncode, returnsignal, output, isTimeout):
         """
